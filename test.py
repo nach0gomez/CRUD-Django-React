eba4ecea8c50628372b043e423dc15e64cf79c43
nach0gomez <terminatitorxdyt@gmail.com>
2023-10-06 11:02:23 -0500

    installing django

---
+++ test.py
@@ -0,0 +1,15 @@
+
+
+def compare_strings(str1, str2):
+    if str1 == str2:
+        return True
+    else:
+        return False
+
+def connect_to_database():
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="yourdatabase"
+    
+    return "connected"
